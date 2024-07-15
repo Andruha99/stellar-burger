e2e1c0928ed1de6bccb05e50e8388af67e4e9a58
Andruha99 <Andriei.korovin.2014@mail.ru>
2024-07-15 12:48:23 +0300

    Add tests for close modal click

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -21,4 +21,42 @@ describe('Tests for burger constructor', () => {
     const ingredient = cy.get('[data-cy="643d69a5c3f7b9001cfa0941"]');
     ingredient.contains('Добавить').click();
   });
+
+  it('open ingredients modal', () => {
+    cy.wait('@getIngredients');
+
+    const ingredientModal = cy
+      .get('[data-cy="643d69a5c3f7b9001cfa0941"]')
+      .click();
+
+    ingredientModal.get('h3').contains('Детали ингредиента');
+  });
+
+  it('close modal by chrest click', () => {
+    cy.wait('@getIngredients');
+
+    const ingredientModal = cy
+      .get('[data-cy="643d69a5c3f7b9001cfa0941"]')
+      .click();
+
+    ingredientModal.should('exist');
+
+    ingredientModal.get('[data-cy="closeButton"]').click();
+
+    ingredientModal.should('not.exist');
+  });
+
+  it('close modal by overlay click', () => {
+    cy.wait('@getIngredients');
+
+    const ingredientModal = cy
+      .get('[data-cy="643d69a5c3f7b9001cfa0941"]')
+      .click();
+
+    ingredientModal.should('exist');
+
+    cy.get('[data-cy="modalOverlay"]').click({ force: true });
+
+    ingredientModal.should('not.exist');
+  });
 });
